ff6e5b3ca539b087414d5c7caad6aeb5d38eed81
LaiaRuizM <laia.ruizm@hotmail.com>
2024-01-21 15:03:44 +0100

    feat: add player and its styles to VideoPlayer

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,8 +1,21 @@
+import styles from "./styles.module.css";
 import React from "react";
-import SRC2 from "../VideoPlayer/coffee.gif";
+import SRC2 from "./coffee.gif";
 
 // const SRC = "../coffee.gif";
 
 export default function VideoPlayer() {
-  return <video type="text/html" src={SRC2} controls={false} />;
+  return (
+    <div>
+      <video
+        autoPlay
+        muted
+        className={styles.video}
+        //   type="text/html"
+        src={SRC2}
+        controls={false}
+      />
+      <button> </button>
+    </div>
+  );
 }
