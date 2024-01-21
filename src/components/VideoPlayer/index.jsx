bb5bafd44c33ac3d88d96df061afe3dc87aba1d4
LaiaRuizM <laia.ruizm@hotmail.com>
2024-01-21 15:13:30 +0100

    feat: add useRef, handlePlay

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,21 +1,32 @@
+import { useRef, useState } from "react"; // To save the references of our video
 import styles from "./styles.module.css";
-import React from "react";
 import SRC2 from "./coffee.gif";
 
 // const SRC = "../coffee.gif";
 
 export default function VideoPlayer() {
+  const [playing, setPlaying] = useState(false);
+  const video = useRef();
+
+  const handlePlay = () => {
+    if (playing) {
+      video.current.stop();
+    } else {
+      video.current.play();
+    }
+    setPlaying(!playing);
+  };
+
   return (
     <div>
       <video
-        autoPlay
-        muted
         className={styles.video}
         //   type="text/html"
         src={SRC2}
         controls={false}
+        ref={video}
       />
-      <button> </button>
+      <button className={styles.player} onChangeCapture={handlePlay} />
     </div>
   );
 }
